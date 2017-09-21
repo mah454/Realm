7db6633094fafbdf4dc39a0a352109c2aa3f1bb8
Mahdi Sheikh Hosseini <mahsom@gmail.com>
2017-09-22 00:01:43 +0330

    Add multiple method usage 1) JDBC 2) JNDI (naming lookup) 3) Annotation Based DataSource

--- basic-servlet/src/main/java/ir/moke/realm/basic/model/DatabaseUtils.java
+++ basic-servlet/src/main/java/ir/moke/realm/basic/model/DatabaseUtils.java
@@ -4,16 +4,9 @@ import javax.annotation.Resource;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
