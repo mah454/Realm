626f52669b8766805337949a74a935272e45695c
Mahdi Sheikh Hosseini <mahsom@gmail.com>
2017-09-22 00:03:50 +0330

    Change method name

--- basic-servlet/src/main/java/ir/moke/realm/basic/setup/ServerInitializer.java
+++ basic-servlet/src/main/java/ir/moke/realm/basic/setup/ServerInitializer.java
@@ -1,6 +1,11 @@
 package ir.moke.realm.basic.setup;
 
+import ir.moke.realm.basic.model.bl.RoleManager;
 import ir.moke.realm.basic.model.bl.UserManager;
+import ir.moke.realm.basic.model.to.Role;
+import ir.moke.realm.basic.model.to.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -8,9 +13,21 @@ import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class ServerInitializer implements ServletContextListener {
-    private UserManager userManager ;
+    private static final Logger logger = LoggerFactory.getLogger(ServerInitializer.class);
+    private UserManager userManager;
+    private RoleManager roleManager;
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        if (adminUserExist()) {
+            logger.debug("Create Admin User");
+            userManager.register(new User("admin", "111111"));
+        }
+
+        if (adminRoleExist()) {
+            logger.debug("Create Admin Role");
+            roleManager.register(new Role("admin", "admin"));
+        }
 
     }
 
@@ -19,8 +36,15 @@ public class ServerInitializer implements ServletContextListener {
 
     }
 
-    private boolean adminExist() {
+    private boolean adminUserExist() {
         userManager = new UserManager();
-        userManager.
+        User user = userManager.find("admin");
+        return user != null;
+    }
+
+    private boolean adminRoleExist() {
+        roleManager = new RoleManager();
+        Role role = roleManager.find("admin");
+        return role != null;
     }
 }
